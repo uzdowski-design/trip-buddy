db6b7dc4893ea8b6c60167cf0249fedd7c022e59
Michal Uzdowski <uzdowski.design@gmail.com>
2021-08-08 10:28:51 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({})
+
+  // get users coordinates on load
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
+    })
+  }, [])
+
+  // update coordinates and bounds when map changes
+  useEffect(() => {
+    if (bounds) {
+      getPlacesData(bounds.sw, bounds.ne)
+        .then((data) => {
+          setPlaces(data);
+        })
+    }
+  }, [bounds, coordinates]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }} >
+        <Grid item xs={12} md={4} >
+          <List places={places} />
+        </Grid>
+        <Grid item xs={12} md={8} >
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,37 @@
+// Map component for generating the map and displaying places
+import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles'
+
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles(); // get styles from styles.js file (written as function)
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={(e) => {
+          // set new coordinates and bounds on change
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+        }}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+
+    </div >
+  )
+}
+
+export default Map;
